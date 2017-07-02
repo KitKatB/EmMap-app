f449a6ea28849d154bddce91185e421c26b82a86
Kit Burgos <kitburgos_17@hotmail.com>
2017-07-03 00:34:43 +1200

    Camera Function
    Implemented camera function

--- XTabs/XTabs.Android/MainActivity.cs
+++ XTabs/XTabs.Android/MainActivity.cs
@@ -6,12 +6,13 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace XTabs.Droid
 {
     [Activity(Label = "XTabs", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    {
+    {   
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -21,7 +22,14 @@ namespace XTabs.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+ 
+
 }
 
